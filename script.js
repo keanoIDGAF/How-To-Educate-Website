926e155ab76072fc7fbc003cccde0581b13026ae
keanoIDGAF <keanoxavi.1209@gmail.com>
2026-02-23 10:58:55 +0100

    basic languages
    html, css, js and why they are made

---
+++ script.js
@@ -0,0 +1,9 @@
+// <!-- ...................................................... -->
+// <!-- Name:                       How To & Educate Website   -->
+// <!-- Why its made:               This website is made for a school project where we need to make a website that is based on the wireframes and mock-ups we made -->
+// <!-- Why this file:              This file is made for the important code that is for you buttons or other things to work -->
+// <!-- ...................................................... -->
+// <!-- Changes      version   -->
+// <!--                        -->
+// <!--                        -->
+// <!-- ...................................................... -->
