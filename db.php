144fc739cb3e2653b2a6822b09aa07b9e61daa27
keanoIDGAF <keanoxavi.1209@gmail.com>
2026-02-23 11:03:34 +0100

    php for database
    php file added for database management

---
+++ db.php
@@ -0,0 +1,9 @@
+<!-- ...................................................... -->
+<!-- Name:                       How To & Educate Website   -->
+<!-- Why its made:               This is made for database management -->
+<!-- Why this file:              This code is the made to connect to databases -->
+<!-- ...................................................... -->
+<!-- Changes      version   -->
+<!--                        -->
+<!--                        -->
+<!-- ...................................................... -->
